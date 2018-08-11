bcafadd5dcccd51dfea2f9212e17253a237790c4
JON <Jcribarro@gmail.com>
2018-08-11 18:30:26 -0400

    FOUND THE ISSUE WITH THE OBJEC NOT BEING SELECTED WHEN CLICKED THE CAMERA WAS PERSPECTIVE INSTEAD OF OTHOGRAPHIC

--- Assets/Scripts/pouringManager.cs
+++ Assets/Scripts/pouringManager.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 
 public class pouringManager : MonoBehaviour {
 
-    static bool isPouring = false;
+    public bool isPouring = false;
     static int totalPours = 0;
     TargetJoint2D pouringJoint;
-
+    Camera mainCam;
 	// Use this for initialization
 	void Start () {
-        pouringJoint = GetComponent<TargetJoint2D>();
-	}
+        //pouringJoint = GetComponent<TargetJoint2D>();
+        mainCam = Camera.main;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-        if(isPouring == true)
-        {
-            Vector3 mousePos = Input.mousePosition;
-            pouringJoint.anchor = mousePos ;
-        }
+      
 
 	}
+    private void FixedUpdate()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var collider = GetComponent<Collider2D>();
+
+            var body = collider.attachedRigidbody;
+
+            pouringJoint = body.gameObject.AddComponent<TargetJoint2D>();
+            pouringJoint.dampingRatio = 1.0f;
+            pouringJoint.frequency = 1;
+
+            pouringJoint.anchor = pouringJoint.transform.InverseTransformPoint(worldPos);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(pouringJoint);
+            pouringJoint = null;
+            return;
+        }
+
+
+        if (pouringJoint)
+        {
+            pouringJoint.target = worldPos;
+            Debug.Log(worldPos);
+            Debug.Log(pouringJoint.target);
+        }
+    }
     private void OnMouseDown()
     {
         isPouring = true;
