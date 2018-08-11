314c84c066f43a045aa142a1bb53ee7e50099a47
JON <Jcribarro@gmail.com>
2018-08-11 11:25:30 -0400

    found a third party asset called liquidPhysics 2D to use for the liquids, for now I coded something up for score and also something to destroy the particles when they get too low below the y axis

---
+++ Assets/Scripts/scoreTaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreGathere : MonoBehaviour {
+
+    BoxCollider2D scoreCollider;
+    int totalScore = 0;
+    int totalPours = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        totalScore += 20 / totalPours;
+    }
+
+
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        totalScore -= 20 / totalPours;
+    }
+
+
+}
